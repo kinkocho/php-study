0a5731352215656d2ad48dd79f1b47fa59d92ef4
kinkocho <cf.zhao86@gmail.com>
2014-03-16 14:02:11 +0900

    merage

--- if.php
+++ if.php
@@ -2,9 +2,17 @@
 <head>
 <title>if</title>
 <body>
-
+<?php
+	$number1 = 100;
+	$number2 = 200;
+	if ($number1 == $number2) {
+		echo "number1‚Ænumber2‚Í“¯‚¶";
+	} else if ($number1 > $number2) {
+		echo "number1‚ÍAnumber2‚æ‚è‘å‚«‚¢!";
+	} else {
+		echo "number1‚ÍAnumber2‚æ‚è¬‚³‚¢!";
+	}
+?>
 </body>
 </head>
 </html>
-<?php
-?>
