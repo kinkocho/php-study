20e0753612148aa059cedd7996f6843eeaa897a5
kinkocho <cf.zhao86@gmail.com>
2014-03-16 13:10:13 +0900

    nothing to say.

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>文字列を表示する</title>
+</head>
+	<body>
+		<?php 
+			echo "abc";
+			print "xyz";
+			
+			$number = 100;
+			echo "<p>変数の値は、$number です。";
+			echo "<p>変数名は、$number です。";
+		?>
+	</body>
+</html>
